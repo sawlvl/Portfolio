62b3322a68f3c4c5af7bbd45db776ee18168fc83
sawlvl <w.sanquer@proton.me>
2026-05-16 23:31:38 +0200

    style : add style pages and list projetcs

---
+++ src/data/avis.js
@@ -0,0 +1,34 @@
+export const avis_sur_moi = [
+
+    {
+        nom: "",
+        status: "",
+        photo: "",
+        avis: "",
+        date: "",
+    },
+
+    {
+        nom: "",
+        status: "",
+        photo: "",
+        avis: "",
+        date: "",
+    },
+
+    {
+        nom: "",
+        status: "",
+        photo: "",
+        avis: "",
+        date: "",
+    },
+
+    {
+        nom: "",
+        photo: "",
+        status: "",
+        avis: "",
+        date: "",
+    },
+]
---
+++ src/data/projects.js
@@ -0,0 +1,46 @@
+export const mesprojets = [
+
+    //Projets: Portfolio
+    {
+        titre: "Portfolio",
+        type: "Projet personnel",
+        description: "Première version de mon portfolio réalisée en parallèle de mes études. Ce projet m’a permis de mettre en pratique mes connaissances en HTML, CSS et JavaScript pour la première fois en les combinant dans un site destiné à exposer mon travail.",
+        status: "primary",
+        couverture: "", //Ajouter premiere image du projets.
+        link: "https://github.com/wilstan-sanquer/portfolio",
+        technologies: ["HTML", "CSS", "JavaScript"],
+    },
+
+    //Projets: SquidTchat
+    { 
+        titre: "SquidTchat",
+        type: "Projet scolaire",
+        description: "Application web de chat en temps réel permettant aux utilisateurs de communiquer via une interface simple et intuitive. Ce projet a été réalisé pour comprendre le fonctionnement d’une messagerie, notamment la gestion des échanges client serveur.",
+        status: "primary",
+        couverture: "../../public/assets/covers/SquidTchat.png",
+        link: "https://github.com/wilstan-sanquer/SquidTchat",
+        technologies: ["HTML", "CSS", "JavaScript", "C++", "Docker"],
+    },
+
+    //Projets: Gravity Z
+    {
+        titre: "Gravity-Z",
+        type: "Projet scolaire",
+        description: "Application interactive permettant de créer une balle, modifier ses paramètres physiques et analyser son comportement (durée, vitesse max, force de contact).",
+        status: "secondary",
+        couverture: "null",
+        link: "https://github.com/wilstan-sanquer/Gravity-Z",
+        technologies: ["HTML", "CSS", "JavaScript", "C++"],
+    },
+
+    //Projets: SubDex
+    {
+        titre: "SubDex",
+        type: "Projet personnel",
+        description: "Application web mobile dédiée à Subnautica centralisant le bestiaire, les ressources et les zones du jeu afin de renseigner les joueurs.",
+        status: "secondary",
+        couverture: "null",
+        link: "", //Ajouter le lien du repo github.
+        technologies: ["HTML", "CSS", "JavaScript"],
+    },
+];
